a23cfc5d166f81928679265258029fe036139104
peterbela <rolikpeti5@gmail.com>
2026-05-08 10:38:16 +0200

    Split chess piece constants into separate file

--- public/script.js
+++ public/script.js
@@ -13,23 +13,6 @@ document.getElementById("checkServer").addEventListener("click", async () => {
   }
 });
 
-// --- Sakkbábu képek (Wikimedia) ---
-const PIECE_IMAGES = {
-  wK: "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg",
-  wQ: "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg",
-  wR: "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg",
-  wB: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
-  wN: "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg",
-  wP: "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg",
-
-  bK: "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg",
-  bQ: "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg",
-  bR: "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg",
-  bB: "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg",
-  bN: "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg",
-  bP: "https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg",
-};
-
 const LESSONS = [
   {
     id: "intro",
@@ -171,22 +154,6 @@ let progressState = {
   lessons: {},      // pl. { pawn: { completedTasks: 1, totalTasks: 1, xpEarned: 10 } }
   lastLessonId: null,
 };
-
-// minden bábu játszik :)
-const piecesList = [
-  "wK",
-  "wQ",
-  "wR",
-  "wB",
-  "wN",
-  "wP",
-  "bK",
-  "bQ",
-  "bR",
-  "bB",
-  "bN",
-  "bP",
-];
 
 // Belső táblaállapot: 8×8, minden elem: null vagy { code, type, color }
 const boardState = Array.from({ length: 8 }, () =>
